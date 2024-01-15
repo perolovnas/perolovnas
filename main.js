c8dcaf91041bdd499641a3b09e03bdf0f5dd9d6f
uxwebb <perolov.nas@oas.nu>
2024-01-15 12:03:47 +0100

    connected to db notion and posts

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './style.css'
+
+async function fetchData() {
+  const posts = await fetch('/api/fetchNotion')
+    .then((res) => res.json())
+    .then((data) => data.results)
+    document.querySelector('main').innerHTML = posts.map((post) => `
+      <article>
+        <h3>${post.properties.Name.title[0].plain_text}</h3>
+      </article>
+    `).join('')
+}
+
+fetchData()
